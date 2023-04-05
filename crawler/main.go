c7b33836312c36a5fbcc19e9c5b781a45d443932
张德志 <zhangdezhi@tungee.com>
2023-04-05 17:09:11 +0800

    feat:并发获取内容

--- crawler/main.go
+++ crawler/main.go
@@ -10,15 +10,13 @@ import (
 
 func httpGet(url string) (result string, err error) {
 	rsp, err := http.Get(url)
-
 	defer rsp.Body.Close()
 
-	//读取网页数据
 	buf := make([]byte, 4096)
 	for {
 		n, err2 := rsp.Body.Read(buf)
 		if n == 0 {
-			fmt.Println("数据读完")
+			fmt.Println("数据读取失败")
 			break
 		}
 		if err2 != nil && err2 != io.EOF {
@@ -30,26 +28,38 @@ func httpGet(url string) (result string, err error) {
 	return
 }
 
+func spiderPage(i int, page chan int) {
+	url := "https://tieba.baidu.com/f?kw=golang&ie=utf-8&pn=" + strconv.Itoa(i+50) + "&pagelets=frs-list%2Fpagelet%2Fthread&pagelets_stamp=1680663932369"
+	result, err := httpGet(url)
+	if err != nil {
+		fmt.Println("获取失败", err)
+	}
+
+	f, err := os.Create("第" + strconv.Itoa(i) + "页数据" + ".html")
+	if err != nil {
+		fmt.Println("写入文件失败")
+		return
+	}
+
+	f.WriteString(result)
+	f.Close()
+
+	page <- i
+
+}
+
 func working(start int, end int) {
 	fmt.Println("开始工作")
 
-	for i := start; i < end; i++ {
-		url := "https://tieba.baidu.com/f?kw=golang&ie=utf-8&pn=" + strconv.Itoa(i+50) + "&pagelets=frs-list%2Fpagelet%2Fthread&pagelets_stamp=1680663932369"
-		result, err := httpGet(url)
-		if err != nil {
-			fmt.Println("获取失败", err)
-		}
+	page := make(chan int)
 
-		f, err := os.Create("第" + strconv.Itoa(i) + "页数据" + ".html")
-		if err != nil {
-			fmt.Println("写入文件失败")
-			return
-		}
+	for i := start; i <= end; i++ {
 
-		f.WriteString(result)
-		f.Close()
+		go spiderPage(i, page)
+	}
 
-		fmt.Println(result)
+	for i := start; i <= end; i++ {
+		fmt.Printf("第%d个页面爬取完成", <-page)
 	}
 
 }
