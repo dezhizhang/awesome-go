174d84bef6e3018ce05dc0af13f923d5d83494d1
张德志 <zhangdezhi@tungee.com>
2023-04-08 22:21:59 +0800

    feat:加载静态资源

--- book/main.go
+++ book/main.go
@@ -15,7 +15,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	// 加载静态资源
+	server := http.FileServer(http.Dir("./assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", server))
+
+	// 路由请求
 	http.HandleFunc("/", handler)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Printf("启动服务失败%s", err)
