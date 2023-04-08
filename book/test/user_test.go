40bbe1603f52c7d365ce0be18b5fc211dd152d7c
张德志 <zhangdezhi@tungee.com>
2023-04-08 15:43:48 +0800

    feat:获取记录

--- book/test/user_test.go
+++ book/test/user_test.go
@@ -2,10 +2,20 @@ package test
 
 import (
 	"book/service"
+	"fmt"
+	"log"
 	"testing"
 )
 
 func TestAddUser(t *testing.T) {
 
 	service.AddUser()
+}
+
+func TestGetUser(t *testing.T) {
+	query, err := service.GetUserQuery()
+	if err != nil {
+		log.Printf("获取结果失败")
+	}
+	fmt.Println(query)
 }
