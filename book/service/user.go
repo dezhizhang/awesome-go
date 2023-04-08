18fc1d1be1d6a7e7ca294235641de36ccd1fa9c7
张德志 <zhangdezhi@tungee.com>
2023-04-08 22:13:21 +0800

    feat:update

--- book/service/user.go
+++ book/service/user.go
@@ -3,7 +3,10 @@ package service
 import (
 	"book/dao"
 	"book/model"
+	"log"
 )
+
+// 检测用户是否存在
 
 func CheckUserIsExist(email string, password string) (*model.User, error) {
 	var user model.User
@@ -14,4 +17,16 @@ func CheckUserIsExist(email string, password string) (*model.User, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+
+// 添加用户
+
+func SaveUser(username string, password string, email string) error {
+	sqlStr := "insert into user(username,password,email) values(?,?,?)"
+	_, err := dao.DB.Exec(sqlStr, username, password, email)
+	if err != nil {
+		log.Printf("添加用户失败%s", err)
+		return err
+	}
+	return nil
 }
