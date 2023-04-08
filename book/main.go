f7134c0eb27e3d89dc63aa3733ff518e583b3733
张德志 <zhangdezhi@tungee.com>
2023-04-08 23:35:31 +0800

    feat:update

--- book/main.go
+++ book/main.go
@@ -11,8 +11,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	must.Execute(w, "")
 }
 
-func handlerLogin(w http.ResponseWriter, r *http.Request) {
+func loginHandler(w http.ResponseWriter, r *http.Request) {
 	must := template.Must(template.ParseFiles("./view/login.html"))
+	must.Execute(w, "")
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	must := template.Must(template.ParseFiles("./view/register.html"))
+	must.Execute(w, "")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	must := template.Must(template.ParseFiles("./view/contact.html"))
+	must.Execute(w, "")
+}
+
+func cartHandler(w http.ResponseWriter, r *http.Request) {
+	must := template.Must(template.ParseFiles("./view/cart.html"))
 	must.Execute(w, "")
 }
 
@@ -24,7 +39,10 @@ func main() {
 
 	// 路由请求
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/login", handlerLogin)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/cart", cartHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
