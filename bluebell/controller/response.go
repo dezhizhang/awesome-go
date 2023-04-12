3c2834ae81175d8da8668510971dc2b91db1e422
zhangdezhi <zhangdezhi@tungee.com>
2023-04-12 22:31:37 +0800

    feat:修改逻辑

--- bluebell/controller/response.go
+++ bluebell/controller/response.go
@@ -6,33 +6,54 @@ import (
 	"net/http"
 )
 
+type ResCode int64
+
 const (
 	CodeSuccess = 100 + iota
 	CodeInvalidParams
+	CodeUserExits
+	CodeUserNotExit
+	CodeInvalidPassword
+	CodeServerBusy
 )
 
-func getCode(code int) string {
-	return ""
+var CodeMsgMap = map[ResCode]string{
+	CodeSuccess:         "请求成功",
+	CodeInvalidParams:   "请求参数错误",
+	CodeUserExits:       "用户已存在",
+	CodeUserNotExit:     "用户不存在",
+	CodeInvalidPassword: "用户名或密码错误",
+	CodeServerBusy:      "服务器繁忙",
+}
+
+func getCode(code ResCode) string {
+	return CodeMsgMap[code]
 }
 
 // 失败返回的数据
 
-func FailHandler(c *gin.Context, code int) {
-	resp := &model.ResponseData{
-		Code: code,
+func ResponseError(c *gin.Context, code ResCode) {
+	c.JSON(http.StatusOK, &model.ResponseData{
+		Code: int(code),
 		Msg:  getCode(code),
 		Data: nil,
-	}
-	c.JSON(http.StatusOK, resp)
+	})
+}
+
+func ResponseErrorWithMsg(c *gin.Context, code ResCode, msg interface{}) {
+	c.JSON(http.StatusOK, &model.ResponseData{
+		Code: int(code),
+		Msg:  msg,
+		Data: nil,
+	})
 }
 
 // 成功后返回的数据
 
-func SuccessHandler(c *gin.Context, code int, data interface{}) {
-	resp := &model.ResponseData{
-		Code: code,
+func ResponseSuccess(c *gin.Context, code ResCode, data interface{}) {
+	c.JSON(http.StatusOK, &model.ResponseData{
+		Code: int(code),
 		Msg:  getCode(code),
 		Data: data,
-	}
-	c.JSON(http.StatusOK, resp)
+	})
 }
--- bluebell/controller/user.go
+++ bluebell/controller/user.go
@@ -4,64 +4,53 @@ import (
 	"bluebell/logic"
 	"bluebell/model"
 	"bluebell/utils"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"log"
-	"net/http"
 )
 
 func RegisterHandler(c *gin.Context) {
-
-	//var reg model.UserRegister
 	userReg := new(model.UserRegister)
 	err := c.ShouldBindJSON(&userReg)
 	if err != nil {
 		errs, ok := err.(validator.ValidationErrors)
 		if !ok {
-			FailHandler(c, 200)
+			ResponseError(c, CodeInvalidParams)
+			return
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"msg": utils.RemoveTopStruct(errs.Translate(utils.Trans)),
-		})
+		ResponseErrorWithMsg(c, CodeInvalidParams,
+			utils.RemoveTopStruct(errs.Translate(utils.Trans)),
+		)
 		return
 	}
-	result, err := logic.CheckUserExit(userReg.Email)
-	if err != nil {
-		log.Printf(err.Error())
-		return
-	}
-	fmt.Println(result)
+	result, _ := logic.CheckUserExit(userReg.Email)
 	if result.Id != 0 {
-		c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "当前用户已存在"})
+		ResponseError(c, CodeUserExits)
 		return
 	}
 
 	err = logic.CreateUser(userReg)
 	if err != nil {
-		c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "注册用户失败"})
+		ResponseError(c, CodeServerBusy)
 		return
 	}
-
-	c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "注册成功", "data": nil})
+	ResponseSuccess(c, CodeSuccess, nil)
 }
 
 func LoginHandler(c *gin.Context) {
 	var loginParams model.UserLogin
 	err := c.ShouldBindJSON(&loginParams)
 	if err != nil {
-		errs, ok := err.(validator.ValidationErrors)
-		if !ok {
-			FailHandler(c, 200)
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"msg": utils.RemoveTopStruct(errs.Translate(utils.Trans)),
-		})
+		errs, _ := err.(validator.ValidationErrors)
+		ResponseErrorWithMsg(c, CodeInvalidParams,
+			utils.RemoveTopStruct(errs.Translate(utils.Trans)),
+		)
 		return
 	}
 	err = logic.UserLogin(&loginParams)
 	if err != nil {
 		log.Printf("login登录失败%s", err)
+		return
 	}
-	c.JSON(http.StatusOK, gin.H{"code": 200, "msg": "登录成功"})
+	ResponseSuccess(c, CodeSuccess, nil)
 }
