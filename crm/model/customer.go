a67b25fccec195d883a1efe20ae95d7ec2715762
zhangdezhi <zhangdezhi@tungee.com>
2023-04-14 09:20:29 +0800

    feat:定义表结构

--- crm/model/customer.go
+++ crm/model/customer.go
@@ -3,10 +3,16 @@ package model
 //原型地址https://www.axureshop.com/ys/1967831
 
 type Customer struct {
-	CustomerName        string `json:"customerName" binding:"required"`     //客户名称
-	CustomerCategory    int    `json:"customerCategory" binding:"required"` //客户类别
-	CustomerIndustry    string `json:"customerIndustry"`                    //客户行业
-	EnterpriseScale     string `json:"enterpriseScale"`                     //企业规模
-	CustomerSource      int    `json:"customerSource"`                      //客户来源
-	CustomerAttribution string `json:"customerAttribution"`                 //客户归属
+	CustomerName        string `json:"customerName" binding:"required"`      //客户名称
+	CustomerCategory    int    `json:"customerCategory" binding:"required"`  //客户类别
+	CustomerIndustry    string `json:"customerIndustry"`                     //客户行业
+	EnterpriseScale     string `json:"enterpriseScale"`                      //企业规模
+	CustomerSource      int    `json:"customerSource"`                       //客户来源
+	CustomerAttribution string `json:"customerAttribution"`                  //客户归属
+	ContactName         string `json:"contactName" binding:"required"`       //系人姓名
+	ContactEmail        string `json:"contactEmail"`                         //电子邮箱
+	ContactNumber       string `json:"contactNumber" binding:"required"`     //联系号码
+	ContactQQ           string `json:"contactQQ" binding:"required"`         //QQ号码
+	ContactDepartment   string `json:"contactDepartment" binding:"required"` //部门职务
+	ContactWeChat       string `json:"contactWeChat" binding:"required"`     //微信账号
 }
